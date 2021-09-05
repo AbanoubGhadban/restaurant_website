eb083cc50acddadb1f2b398d55d64152eb3c7968
Abanoub Ghadban <abanoub.gdb@gmail.com>
2021-09-05 17:13:15 +0200

    added home slider

--- js/script.js
+++ js/script.js
@@ -20,3 +20,18 @@ searchIcon.onclick = () => {
 searchForm.querySelector('#close').onclick = () => {
     searchForm.classList.remove('active');
 }
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop:true,
+});
